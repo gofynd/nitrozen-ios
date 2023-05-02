abbac8cc2227f803863fa61f37c7667c2055eb86
rushangfynd <rushangprajapati@fynd.com>
2023-05-02 16:43:09 +0530

    Condition improve for increment and decrement

--- Sources/Nitrozen-SwiftUI/Elements/Stepper/NitrozenStepperView.swift
+++ Sources/Nitrozen-SwiftUI/Elements/Stepper/NitrozenStepperView.swift
@@ -40,9 +40,9 @@ public struct NitrozenStepperView: View {
     
     public var body: some View {
         HStack(spacing: self.itemSpacing){
-            decrementAction()
+			decrementActionButton()
             inputTextfield()
-            incrementAction()
+            incrementActionButton()
         }
     }
     
@@ -73,122 +73,94 @@ public struct NitrozenStepperView: View {
     }
     
     @ViewBuilder
-    func incrementAction() -> some View{
-        switch appearance.incrementButton{
-        case .systemImage(let imageName):
-            incremenButton(imageType: .systemImage(name: imageName))
-        case .assetImage(let imageName):
-            incremenButton(imageType: .assetImage(name: imageName))
-        }
+    func incrementActionButton() -> some View{
+		let isButtonDisable = (maxInputValue == intInputValue)
+		
+		let borderColor = isButtonDisable ?
+		appearance.actionButton.borderColorDisabled : appearance.actionButton.borderColor
+				
+		let titleColor = isButtonDisable ?
+		appearance.actionButton.titleColorDisabled : appearance.actionButton.titleColor
+
+		let backGroundColor = isButtonDisable ?
+		appearance.actionButton.backgroundColorDisabled : appearance.actionButton.backgroundColor
+		
+		Button(action: {
+			increment()
+		}) {
+			Group{
+				switch appearance.incrementButton {
+				case .systemImage(let name):
+					Image(systemName: name)
+				case .assetImage(let name):
+					Image(name)
+				}
+			}
+			.frame(
+				width: appearance.actionButtonSize.height,
+				height: appearance.actionButtonSize.height
+			)
+		}
+		.foregroundColor(titleColor)
+		.disabled(isButtonDisable)
+		.background(backGroundColor)
+		.apply(
+			shape: appearance.viewShpae,
+			color:borderColor,
+			lineWidth: appearance.actionButton.borderWidth
+		)
+
     }
     
     @ViewBuilder
-    func incremenButton(imageType:NitrozenAppearance.StepperView.CustomView) -> some View{
-        let isButtonDisable = (maxInputValue == intInputValue)
+	func decrementActionButton() -> some View{
+		let isButtonDisable = (minInputValue == intInputValue)
+		
+		let borderColor = isButtonDisable ?
+		appearance.actionButton.borderColorDisabled : appearance.actionButton.borderColor
+		
+		let titleColor = isButtonDisable ?
+		appearance.actionButton.titleColorDisabled : appearance.actionButton.titleColor
+		
+		let backGroundColor = isButtonDisable ?
+		appearance.actionButton.backgroundColorDisabled : appearance.actionButton.backgroundColor
+		
+		Button(action: {
+			decrement()
+		}) {
+			Group{
+				switch appearance.decrementButton {
+				case .systemImage(let name):
+					Image(systemName: name)
+				case .assetImage(let name):
+					Image(name)
+				}
+			}
+			.frame(
+				width: appearance.actionButtonSize.height,
+				height: appearance.actionButtonSize.height
+			)
+		}
+		.foregroundColor(titleColor)
+		.background(backGroundColor)
+		.disabled(isButtonDisable)
+		.apply(
+			shape: appearance.viewShpae,
+			color:borderColor,
+			lineWidth: appearance.actionButton.borderWidth
+		)
+	}
         
-        let borderColor = isButtonDisable ?
-        appearance.actionButton.borderColorDisabled : appearance.actionButton.borderColor
-                
-        let titleColor = isButtonDisable ?
-        appearance.actionButton.titleColorDisabled : appearance.actionButton.titleColor
-
-        let backGroundColor = isButtonDisable ?
-        appearance.actionButton.backgroundColorDisabled : appearance.actionButton.backgroundColor
-        
-        Button(action: {
-            increment()
-        }) {
-            Group{
-                switch imageType {
-                case .systemImage(let name):
-                    Image(systemName: name)
-                case .assetImage(let name):
-                    Image(name)
-                }
-            }
-            .frame(
-                width: appearance.actionButtonSize.height,
-                height: appearance.actionButtonSize.height
-            )
-        }
-        .foregroundColor(titleColor)
-        .disabled(isButtonDisable)
-        .background(backGroundColor)
-        .apply(
-            shape: appearance.viewShpae,
-            color:borderColor,
-            lineWidth: appearance.actionButton.borderWidth
-        )
-        
-    }
-    
-    @ViewBuilder
-    func decrementAction() -> some View{
-        switch appearance.decrementButton {
-        case .systemImage(let imageName):
-            decrementButton(imageType: .systemImage(name: imageName))
-        case .assetImage(let imageName):
-            decrementButton(imageType: .assetImage(name: imageName))
-        }
-    }
-    
-    @ViewBuilder
-    func decrementButton(imageType:NitrozenAppearance.StepperView.CustomView) -> some View{
-        let isButtonDisable = (minInputValue == intInputValue)
-
-        let borderColor = isButtonDisable ?
-        appearance.actionButton.borderColorDisabled : appearance.actionButton.borderColor
-        
-        let titleColor = isButtonDisable ?
-        appearance.actionButton.titleColorDisabled : appearance.actionButton.titleColor
-
-        let backGroundColor = isButtonDisable ?
-        appearance.actionButton.backgroundColorDisabled : appearance.actionButton.backgroundColor
-        
-        Button(action: {
-            decrement()
-        }) {
-            Group{
-                switch imageType {
-                case .systemImage(let name):
-                    Image(systemName: name)
-                case .assetImage(let name):
-                    Image(name)
-                }
-            }
-            .frame(
-                width: appearance.actionButtonSize.height,
-                height: appearance.actionButtonSize.height
-            )
-        }
-        .foregroundColor(titleColor)
-        .background(backGroundColor)
-        .disabled(isButtonDisable)
-        .apply(
-            shape: appearance.viewShpae,
-            color:borderColor,
-            lineWidth: appearance.actionButton.borderWidth
-        )
-    }
-    
     func increment(){
-        if intInputValue < maxInputValue{
-            var value = intInputValue
-            value += step
-            inputValue = min(value, self.maxInputValue).description
-        }
-        
+		var value = intInputValue
+		value += step
+		inputValue = min(value, self.maxInputValue).description
     }
     
     func decrement(){
-        if minInputValue < intInputValue{
-            var value = intInputValue
-            value -= step
-            inputValue = max(value, self.minInputValue).description
-        }
-        else{
-            inputValue = max(Int(inputValue) ?? 00, self.minInputValue).description
-        }
+		var value = intInputValue
+		value -= step
+		inputValue = max(value, self.minInputValue).description
     }
     
 }
